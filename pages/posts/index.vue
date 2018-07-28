d59d89848147bb19175dae46d844c49b1856701c
Amy Em <amy@systemexperts.asia>
2018-07-28 12:38:56 +0700

    first commit

---
+++ pages/posts/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+      hello
+      <wp />
+    </div>
+  </section>
+</template>
+
+<script>
+import wp from '~/lib/wp.js'
+
+export default {
+  async asyncData ({ params }) {
+    return wp.posts()
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 1rem auto;
+  width: 90%;
+  max-width: 900px;
+}
+</style>
